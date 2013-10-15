3a4835e5f6a4e6a95a18aa340c7d2f7a08fe039b
Heath Seals <heath@nodata.org>
2013-10-14 21:37:31 -0500

    grab all the certs

--- pkild-api.rb
+++ pkild-api.rb
@@ -3,6 +3,21 @@ require 'json'
 require 'sinatra'
 
 set :bind, '0.0.0.0'
+ 
+get '/api/all' do
+  crtfiles = File.join("/home/heathseals/pkild-crts/**", "*.crt")
+  all =  Dir.glob(crtfiles)
+  array = []
+  all.each do |x|
+    x.each_line do |line|
+      raw = File.read line
+      cert = OpenSSL::X509::Certificate.new raw
+      array << {:subject => "#{cert.subject}", :issuer => "#{cert.issuer}", :not_before => "#{cert.not_before}", :not_after => "#{cert.not_after}"}
+    end
+  end
+  array.group_by {|d| d[:subject]}
+  JSON.pretty_generate(array)
+end
 
 get '/api/:type/:name' do
   content_type :json
