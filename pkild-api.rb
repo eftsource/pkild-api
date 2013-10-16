e692e7f9727c68e3a63a223a92a02508ecce8942
Heath Seals <heathseals@eftsource.com>
2013-10-16 11:15:04 -0500

    refactor some code and add days_left

--- pkild-api.rb
+++ pkild-api.rb
@@ -10,21 +10,19 @@ eftsource = "/home/heathseals/pkild-api/eftsource/crts"
  
 get '/api/all/:type' do
   content_type :json
-  crtfiles = File.join("#{eftroot}/**", "*.crt")
-  crtpath =  Dir.glob(crtfiles)
-  all = []
+  crtpath =  Dir.glob(File.join("#{eftroot}/**", "*.crt"))
   valid = []
   expired = []
   crtpath.each do |x|
     x.each_line do |line|
-      raw = File.read line
-      cert = OpenSSL::X509::Certificate.new raw
-      subjectcn = cert.subject.to_s.match(/CN=(.*)\//)[1]
-      issuercn = cert.issuer.to_s.match(/CN=(.*)\//)[1]
+      cert = OpenSSL::X509::Certificate.new(File.read line)
+      subject_cn = cert.subject.to_s.match(/CN=(.*)\//)[1]
+      issuer_cn = cert.issuer.to_s.match(/CN=(.*)\//)[1]
+      days_left = DateTime.parse("#{cert.not_after}").mjd - DateTime.now.mjd
       if cert.not_after < Time.now
-        expired << {:valid => "false", :subjectcn => "#{subjectcn}", :issuercn => "#{issuercn}", :not_before => "#{cert.not_before}", :not_after => "#{cert.not_after}"}
+        expired << {:valid => "false", :subject_cn => "#{subject_cn}", :issuer_cn => "#{issuer_cn}", :not_before => "#{cert.not_before}", :not_after => "#{cert.not_after}", :days_left => ("#{days_left}")}
       else
-        valid << {:valid => "true", :subjectcn => "#{subjectcn}", :issuercn => "#{issuercn}", :not_before => "#{cert.not_before}", :not_after => "#{cert.not_after}"}
+        valid << {:valid => "true", :subject_cn => "#{subject_cn}", :issuer_cn => "#{issuer_cn}", :not_before => "#{cert.not_before}", :not_after => "#{cert.not_after}", :days_left => ("#{days_left}")}
       end
     end
   end
@@ -41,12 +39,12 @@ get '/api/all/:type' do
    JSON.pretty_generate(sorted)
   when 'next'
    sorted = valid.sort_by {|h| h[:not_after]}
-   expiresnext = sorted[0]
-   JSON.pretty_generate(expiresnext)
+   expires_next = sorted[0]
+   JSON.pretty_generate(expires_next)
   when 'nextten'
    sorted = valid.sort_by {|h| h[:not_after]}
-   nextten = sorted[0..9]
-   JSON.pretty_generate(nextten)
+   next_ten = sorted[0..9]
+   JSON.pretty_generate(next_ten)
   else
     "#{params[:type]} not implemented"
   end
@@ -56,13 +54,14 @@ get '/api/:type/:name' do
   content_type :json
   if "#{params[:type]}" == 'host' 
     domain = "eftdomain.net"
-    raw = File.read "#{eftdomain}/#{params[:name]}.#{domain}/#{params[:name]}.#{domain}.crt"
+    cert = OpenSSL::X509::Certificate.new(File.read "#{eftdomain}/#{params[:name]}.#{domain}/#{params[:name]}.#{domain}.crt")
+    subject_cn = cert.subject.to_s.match(/CN=(.*)\//)[1]
   end
   if "#{params[:type]}" == 'person' 
-    raw = File.read "#{eftsource}/#{params[:name]}/#{params[:name]}.crt"
+    cert = OpenSSL::X509::Certificate.new(File.read "#{eftsource}/#{params[:name]}/#{params[:name]}.crt")
+    subject_cn = cert.subject.to_s.match(/CN=(.*)\//)[1]
   end
-  cert = OpenSSL::X509::Certificate.new raw
-  hash = {:id => "#{params[:name]}", :type => "#{params[:type]}", :subject => "#{cert.subject}", :issuer => "#{cert.issuer}", :not_before => "#{cert.not_before}", :not_after => "#{cert.not_after}"}
+  hash = {:subject_cn => "#{subject_cn}", :type => "#{params[:type]}", :subject => "#{cert.subject}", :issuer => "#{cert.issuer}", :not_before => "#{cert.not_before}", :not_after => "#{cert.not_after}"}
   JSON.pretty_generate(hash) 
 end
 
@@ -70,24 +69,25 @@ get '/api/:type/:name/:command' do
   content_type :json
   if "#{params[:type]}" == 'host' 
     domain = "eftdomain.net"
-    raw = File.read "#{eftdomain}/#{params[:name]}.#{domain}/#{params[:name]}.#{domain}.crt"
+    cert = OpenSSL::X509::Certificate.new(File.read "#{eftdomain}/#{params[:name]}.#{domain}/#{params[:name]}.#{domain}.crt")
+    subject_cn = cert.subject.to_s.match(/CN=(.*)\//)[1]
   end
   if "#{params[:type]}" == 'person' 
-    raw = File.read "#{eftsource}/#{params[:name]}/#{params[:name]}.crt"
+    cert = OpenSSL::X509::Certificate.new(File.read "#{eftsource}/#{params[:name]}/#{params[:name]}.crt")
+    subject_cn = cert.subject.to_s.match(/CN=(.*)\//)[1]
   end
-  cert = OpenSSL::X509::Certificate.new raw
   case "#{params[:command]}"
   when 'issuer'
-    hash = {:id => "#{params[:name]}", :type => "#{params[:type]}", :issuer => "#{cert.issuer}"}
+    hash = {:subject_cn => "#{subject_cn}", :type => "#{params[:type]}", :issuer => "#{cert.issuer}"}
     JSON.pretty_generate(hash) 
   when 'not_after'
-    hash = {:id => "#{params[:name]}", :type => "#{params[:type]}", :not_after => "#{cert.not_after}"}
+    hash = {:subject_cn => "#{subject_cn}", :type => "#{params[:type]}", :not_after => "#{cert.not_after}"}
     JSON.pretty_generate(hash) 
   when 'not_before'
-    hash = {:id => "#{params[:name]}", :type => "#{params[:type]}", :not_before => "#{cert.not_before}"}
+    hash = {:subject_cn => "#{subject_cn}", :type => "#{params[:type]}", :not_before => "#{cert.not_before}"}
     JSON.pretty_generate(hash) 
   when 'subject'
-    hash = {:id => "#{params[:name]}", :type => "#{params[:type]}", :subject => "#{cert.subject}"}
+    hash = {:subject_cn => "#{subject_cn}", :type => "#{params[:type]}", :subject => "#{cert.subject}"}
     JSON.pretty_generate(hash) 
   else
     "#{params[:command]} not implemented"
--- scratch.rb
+++ scratch.rb
@@ -1,29 +1,28 @@
 require 'openssl'
 require 'json'
+require 'time'
 
-crtfiles = File.join("/home/heathseals/pkild-api/**", "*.crt")
-crtpath =  Dir.glob(crtfiles) 
 expired = []
 valid = []
-all = []
-subjectcn = []
-issuercn = []
+
+#crtfiles = File.join("/home/heathseals/pkild-api/**", "*.crt")
+crtpath =  Dir.glob(File.join("/home/heathseals/pkild-api/**", "*.crt")) 
 crtpath.each do |x|
   x.each_line do |line|
-    subjectcn = []
-    raw = File.read line 
-    cert = OpenSSL::X509::Certificate.new raw
-    subjectcn = cert.subject.to_s.match(/CN=(.*)\//)[1]
-    issuercn = cert.issuer.to_s.match(/CN=(.*)\//)[1]
+    cert = OpenSSL::X509::Certificate.new(File.read line)
+    subject_cn = cert.subject.to_s.match(/CN=(.*)\//)[1]
+    issuer_cn = cert.issuer.to_s.match(/CN=(.*)\//)[1]
+    days_left = DateTime.parse("#{cert.not_after}").mjd - DateTime.now.mjd
     if cert.not_after < Time.now 
-      expired << {:valid => "false", :subjectcn => "#{subjectcn}", :issuercn => "#{issuercn}", :not_before => "#{cert.not_before}", :not_after => "#{cert.not_after}"}
+      expired << {:valid => "false", :subject_cn => "#{subject_cn}", :issuer_cn => "#{issuer_cn}", :not_before => "#{cert.not_before}", :not_after => "#{cert.not_after}", :days_left => ("#{days_left}")}
     else
-      valid << {:valid => "true", :subjectcn => "#{subjectcn}", :issuercn => "#{issuercn}", :not_before => "#{cert.not_before}", :not_after => "#{cert.not_after}"}
+      valid << {:valid => "true", :subject_cn => "#{subject_cn}", :issuer_cn => "#{issuer_cn}", :not_before => "#{cert.not_before}", :not_after => "#{cert.not_after}", :days_left => ("#{days_left}")}
     end
   end
 end
-#all = expired + valid
+
+all = expired + valid
 sorted = valid.sort_by {|h| h[:not_after]}
-expiresnext = sorted[0] 
-nextten = sorted[0..9]
-puts JSON.pretty_generate(expiresnext)
+expires_next = sorted[0] 
+next_ten = sorted[0..9]
+puts JSON.pretty_generate(next_ten)
